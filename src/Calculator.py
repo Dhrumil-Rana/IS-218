668c2d7ad3be38f8a238accd42d41ad097235d54
DhrumilRana1998 <dgr25@njit.edu>
2021-06-25 19:56:32 -0400

    feature: create calculator and test

---
+++ src/Calculator.py
@@ -0,0 +1,11 @@
+def printf(param):
+    pass
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+        printf("ok")
+
---
+++ src/CalculatorTest.py
@@ -0,0 +1,13 @@
+import unittest
+from Calculator import Calculator
+from pprint import pprint
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(self.calculator, Calculator)
+
+if __name__ == '__main__':
+    unittest.main()
