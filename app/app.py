993fee834f38368d78abe8966463a7610686d4bf
DhrumilRana1998 <dgr25@njit.edu>
2021-07-19 22:59:29 -0400

    Added upto lesson 2

--- app/app.py
+++ app/app.py
@@ -1,43 +1,117 @@
 from typing import List, Dict
 import mysql.connector
+from flaskext.mysql import MySQL
 import simplejson as json
-from flask import Flask, Response
+from flask import Flask, Response, request, redirect
 from flask import render_template
+from pymysql.cursors import DictCursor
+
 
 app = Flask(__name__)
+mysql = MySQL(cursorclass=DictCursor)
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'homework'
+mysql.init_app(app)
 
 
-def cities_import() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'citiesData'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor(dictionary=True)
-
-    cursor.execute('SELECT * FROM tblCitiesImport')
-    result = cursor.fetchall()
-
-    cursor.close()
-    connection.close()
-
-    return result
-
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     user = {'username': 'Dhrumil'}
-    cities_data = cities_import()
-    return render_template('index.html', title='Home', user=user, cities=cities_data)
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM hw_200')
+    result = cursor.fetchall()
+    return render_template('index.html', title='Home', user=user, people=result)
 
 
-@app.route('/api/cities')
-def cities() -> str:
-    js = json.dumps(cities_import())
-    resp = Response(js, status=200, mimetype='application/json')
+@app.route('/view/<int:person_id', methods=['GET'])
+def record_view(person_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM hw_200 WHERE `Index`=%s', person_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', city=result[0])
+
+
+@app.route('/edit/<int:person_id>', methods=['GET'])
+def form_edit_get(person_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM hw_200 WHERE `Index`=%s', person_id)
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', city=result[0])
+
+
+@app.route('/edit/<int:person_id>', methods=['POST'])
+def form_update_post(person_id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('Height_Inches'), request.form.get('Weight_Pounds'), person_id)
+    sql_update_query = """UPDATE hw_200 t SET t.Height_Inches = %s, t.Weight_Pounds = %s WHERE t.Index = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/people/new', methods=['GET'])
+def form_insert_get():
+    return render_template('new.html', title='New person Form')
+
+
+@app.route('/people/new', methods=['POST'])
+def form_insert_post():
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('Height_Inches'), request.form.get('Weight_Pounds'))
+    sql_insert_query = """INSERT INTO hw_200 (Height_Inches, Weight_Pounds) VALUES (%s, %s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/delete/<int:person_id>', methods=['POST'])
+def form_delete_post(city_id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM hw_200 WHERE `Index` = %s """
+    cursor.execute(sql_delete_query, city_id)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/api/v1/people', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM hw_200')
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/people/<int:person_id>', methods=['GET'])
+def api_retrieve(person_id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM hw_200 WHERE `Index`=%s', person_id)
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/people/', methods=['POST'])
+def api_add() -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/people/<int:person_id>', methods=['PUT'])
+def api_edit(city_id) -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/people/<int:person_id>', methods=['DELETE'])
+def api_delete(person_id) -> str:
+    resp = Response(status=210, mimetype='application/json')
     return resp
 
 
